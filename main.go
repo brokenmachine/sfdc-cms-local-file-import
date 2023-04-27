38fc56475d20be6242453342789d66374f84e825
Duane Taylor <dataylor1975@gmail.com>
2023-04-27 16:50:52 -0700

    starting to support document content type

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
-// JSON file format info:
-// - https://help.salesforce.com/s/articleView?id=sf.cms_import_content_json.htm&type=5
-// - https://help.salesforce.com/s/articleView?id=sf.cms_import_export_overview.htm&type=5
+/* JSON file format info:
+/- https://help.salesforce.com/s/articleView?id=sf.cms_import_content_json.htm&type=5
+ - https://help.salesforce.com/s/articleView?id=sf.cms_import_export_overview.htm&type=5
+*/
 
 package main
 
@@ -46,40 +47,54 @@ func isImageTypeSupported(fileName string) bool {
 	return matched
 }
 
-func isValidFile(file os.DirEntry, cmsImportDir string) bool {
+func isValidFile(file os.DirEntry, cmsImportDir string, importType string) bool {
 	if file.IsDir() {
 		reportErrorStdOut(file.Name() + " is a directory, skipping\n")
 		return false
 	}
 
 	// per SFDC docs, image files cannot be >25MB
-	fileInfo, _ := os.Lstat(cmsImportDir + "/_media/" + file.Name())
-	if fileInfo.Size() > 2.5e+7 {
-		reportErrorStdOut(file.Name() + " is greater than 25MB, skipping\n")
-		return false
+	if importType == "cms_image" {
+		fileInfo, _ := os.Lstat(cmsImportDir + "/_media/" + file.Name())
+		if fileInfo.Size() > 2.5e+7 {
+			reportErrorStdOut(file.Name() + " is greater than 25MB, skipping\n")
+			return false
+		}
+
+		if !isImageTypeSupported(file.Name()) {
+			reportErrorStdOut(file.Name() + " is of a non-supported file type, skipping\n")
+			return false
+		}
 	}
 
-	if !isImageTypeSupported(file.Name()) {
-		reportErrorStdOut(file.Name() + " is of a non-supported file type, skipping\n")
-		return false
-	}
 	return true
 }
 
 func main() {
 	args := os.Args
+	if len(args) != 3 {
+		reportErrorStdOut("Usage: cms-import <CMS import directory> <cms_image|cms_document>")
+		os.Exit(2)
+	}
+
 	cmsImportDir := args[1]
-	jsonFilename := args[2]
+	importType := args[2]
+	jsonFilename := "content.json"
+	if importType != "cms_image" && importType != "cms_document" {
+		reportErrorStdOut("Import type (2nd parameter) must be cms_image or cms_document, exiting")
+		os.Exit(2)
+	}
+
 	files, err := os.ReadDir(cmsImportDir + "/_media")
 	if err != nil {
 		panic(err)
 	}
 
 	for _, file := range files {
-		if isValidFile(file, cmsImportDir) {
+		if isValidFile(file, cmsImportDir, importType) {
 			fileNameWithoutExtension := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
 			contentItem := ContentItem{
-				ContentType: "cms_image",
+				ContentType: importType,
 				UrlName:     fileNameWithoutExtension,
 				ContentBody: ContentBody{
 					Title:         fileNameWithoutExtension,
@@ -88,7 +103,6 @@ func main() {
 				},
 			}
 			contentItems = append(contentItems, contentItem)
-
 		}
 	}
 
